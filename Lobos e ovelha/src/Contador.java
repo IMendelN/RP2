fd1c561d0211314de8cf03e628a1ba1f07d9f327
Elanderson Cavalcanti Gomes <elanderson.cg@gmail.com>
2021-11-22 23:45:37 +0000

    Atualização Classe Contador

--- Lobos e ovelha/src/Contador.java
+++ Lobos e ovelha/src/Contador.java
@@ -1,33 +1,21 @@
 //import java.awt.Color;
 
-public class Contador
-{
+//Nada muito grnade aqui não, só organização mesmo.
+
+public class Contador{
     private String name;
     private int count;
-//ok
-    public Contador(String name)
-    {
+    
+    public Contador(String name){
         this.name = name;
         count = 0;
     }
-//ok    
-    public String getName()
-    {
-        return name;
-    }
-//ok
-    public int getCount()
-    {
-        return count;
-    }
-//ok
-    public void increment()
-    {
-        count++;
-    }
-//ok    
-    public void reset()
-    {
-        count = 0;
-    }
+    
+    public String getName(){return name;}
+
+    public int getCount(){return count;}
+
+    public void increment(){count++;}
+   
+    public void reset(){count = 0;}
 }
