9259e0226a1a73c563f7ff471240aa894b7d19e7
Iago Mendes Nogueira <iagomwndes432@gmail.com>
2021-11-22 17:46:32 +0000

    Correção e comentários das classes Randomizador e Localizacao.

--- Lobos e ovelha/src/Localizacao.java
+++ Lobos e ovelha/src/Localizacao.java
@@ -1,15 +1,15 @@
-
+//ok
 public class Localizacao
 {
     private int linha;
     private int coluna;
-
+//ok
     public Localizacao(int linha, int coluna)
     {
         this.linha = linha;
         this.coluna = coluna;
     }
-    
+//ok
     public boolean equals(Object obj)
     {
         if(obj instanceof Localizacao) {
@@ -20,22 +20,22 @@ public class Localizacao
             return false;
         }
     }
-    
+//ok    
     public String toString()
     {
         return linha + "," + coluna;
     }
-    
+//ok 
     public int hashCode()
     {
         return (linha << 16) + coluna;
     }
-    
+//ok
     public int getLinha()
     {
         return linha;
     }
-    
+//ok  
     public int getColuna()
     {
         return coluna;
--- Lobos e ovelha/src/Randomizador.java
+++ Lobos e ovelha/src/Randomizador.java
@@ -1,11 +1,11 @@
 import java.util.Random;
-
+//ok
 public class Randomizador
 {
     private static final int SEED = 1111;
     private static final Random rand = new Random(SEED);
     private static final boolean useShared = true;
-
+//ok
     public static Random getRandom()
     {
         if(useShared) {
@@ -15,7 +15,7 @@ public class Randomizador
             return new Random();
         }
     }
-    
+//ok
     public static void reset()
     {
         if(useShared) {
