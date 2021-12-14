9acadaeed1441492d1d8c33b80c6a016a3b7b4dd
Dionas Luan Muller <dionasluanmuller3@gmail.com>
2021-12-13 21:43:04 -0300

    Criação de arquivos para o Adriel poder enviar pois o git dele bugou

---
+++ Lobos e ovelha/src/SimuladorTelaTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimuladorTelaTest {
+
+	@Test
+	void testSimuladorTela() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostraStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEhViavel() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Lobos e ovelha/src/SimuladorTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimuladorTest {
+
+	@Test
+	void testSimulador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSimuladorIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExecutaLongaSimulacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSimulacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSimulacaoUmaEtapa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRedefine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEtapa() {
+		fail("Not yet implemented");
+	}
+
+}
